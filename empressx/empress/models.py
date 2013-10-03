ccd455da06d3cf536920187cfcdef7cd67a38ead
sveinchen <sveinchen@gmail.com>
2013-10-03 15:50:52 +0800

    first commit

---
+++ empressx/empress/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Application(models.Model):
+
+    name = models.CharField(unique=True)
+    alias = models.CharField(blank=True, default='')
+    svn_path = models.CharField()
+    project_path = models.CharField()
+    wsgi_handler = models.CharField()
+
+    def __unicode__(self):
+        if self.alias:
+            return unicode("{}({})".format(self.alias,
+                                           self.name))
+        else:
+            return self.name
+
+
+class Server(models.Model):
+
+    is_active = models.BooleanField(default=True)
+
+
+class Environment(models.Model):
+
+    application = models.ForeignKey(Application)
+    key = models.CharField()
+    value = models.TextField()
+
+
+class Relationship(models.Model):
+
+    application = models.ForeignKey(Application)
+    server = models.ForeignKey(Server)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(default=timezone.now())
+    date_deprecated = models.DateTimeField(null=True)
+
+    def __unicode__(self):
+        if self.is_active:
+            state = 'ACTIVE'
+        else:
+            state = 'INACTIVE'
+
+        return unicode("[{}] {}@{}".format(state,
+                                           self.application,
+                                           self.server))
