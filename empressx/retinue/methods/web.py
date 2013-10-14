b0610058bcf69a4499e8f8f564b2f203d7bbd0ce
sveinchen <sveinchen@gmail.com>
2013-10-14 12:02:56 +0800

    first release

--- empressx/retinue/methods/web.py
+++ empressx/retinue/methods/web.py
@@ -1,6 +1,11 @@
+from empressx.retinue.tasks import web
+
+
 def serve(app_name, uuid):
+    web.serve.apply_async(args=(app_name, uuid))
     return True
 
 
 def unserve(app_name, uuid):
+    web.unserve.apply_async(args=(app_name, uuid))
     return True
