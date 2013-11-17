db1c3da8adbf26863575094e946c7b883e619d28
sveinchen <sveinchen@gmail.com>
2013-11-17 16:23:47 +0800

    first release

--- empressx/contrib/auth/urls.py
+++ empressx/contrib/auth/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('bpm.contrib.auth.views',
+urlpatterns = patterns('empressx.contrib.auth.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
 )
